1af8571a0b6b719b5b7e5b46091e920464fa2056
Daniel García Aubert <dgaubert@carto.com>
2017-01-27 21:23:56 +0100

    Improve test

--- src/test/acceptance/app-test.js
+++ src/test/acceptance/app-test.js
@@ -2,6 +2,8 @@ import assert from 'assert'
 import fetch from 'node-fetch'
 import HelloWorld from '../../example/hello-world'
 
+const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+
 describe('end-to-end app examples', function () {
   before(function () {
     this.app = new HelloWorld({ port: 0 })
@@ -27,16 +29,19 @@ describe('end-to-end app examples', function () {
     assert.equal(body, 'Hello World')
   })
 
-  it('GET / should response 200 ok', async function () {
+  it('GET / should response with the given x-request-id header', async function () {
     const res = await fetch(`http://localhost:${this.port}/`, {
       headers: { 'x-request-id': 'wadus' }
     })
-    const body = await res.text()
 
     assert.ok(res.ok)
-    assert.equal(res.status, 200)
     assert.equal(res.headers.get('x-request-id'), 'wadus')
+  })
 
-    assert.equal(body, 'Hello World')
+  it('GET / should response with x-request-id header', async function () {
+    const res = await fetch(`http://localhost:${this.port}/`)
+
+    assert.ok(res.ok)
+    assert.ok(res.headers.get('x-request-id').match(UUID_REGEX))
   })
 })
