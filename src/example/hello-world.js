cc153365bd2ac38e2b5b7453a8cb62090726c597
Daniel García Aubert <dgaubert@carto.com>
2017-02-13 23:32:23 +0100

    Improve example

--- src/example/hello-world.js
+++ src/example/hello-world.js
@@ -1,23 +1,19 @@
 import Nitro from '../'
-import Router from 'koa-router'
 
 export default class HelloWorld {
   constructor (options) {
     this.nitro = new Nitro(options)
 
     if (this.nitro.app) {
-      const router = Router()
       const body = new Buffer('Hello World')
       const message = body.toString('utf8')
 
-      router.get('/', async (ctx) => {
+      this.nitro.app.use(ctx => {
         ctx.log.info(message)
         ctx.metrics.increment('hello_world')
         ctx.set('content-type', 'text/html')
         ctx.body = body
       })
-
-      this.nitro.app.use(router.routes())
     }
   }
 
