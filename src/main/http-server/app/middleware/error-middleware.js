86c9631ac099e2e6c54c32065be49581901ea418
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-03-06 23:35:14 +0100

    Use context logger

--- src/main/http-server/app/middleware/error-middleware.js
+++ src/main/http-server/app/middleware/error-middleware.js
@@ -1,22 +1,13 @@
 import MiddlewareInterface from './middleware-interface'
 
 export default class ErrorMiddleware extends MiddlewareInterface {
-  constructor (logger) {
-    super()
-    this.logger = logger
-  }
-
   regist (app) {
     app.use(async (ctx, next) => {
       try {
         await next()
       } catch (err) {
-        this.logger.error(err)
-        // will only respond with JSON
-        ctx.status = err.statusCode || err.status || 500
-        ctx.body = {
-          message: err.message
-        }
+        ctx.log.error(err)
+        throw err // delegates to koa's default error middleware
       }
     })
   }
