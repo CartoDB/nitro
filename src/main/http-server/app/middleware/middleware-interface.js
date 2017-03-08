b136187946da6c8ff23a5e9ef2dab383ca943fb5
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-03-08 19:10:42 +0100

    Use error-messages

--- src/main/http-server/app/middleware/middleware-interface.js
+++ src/main/http-server/app/middleware/middleware-interface.js
@@ -1,15 +1,17 @@
+import ErrorMessage from './utils/error-message'
+
 export default class MiddlewareInterface {
   constructor () {
     if (new.target === MiddlewareInterface) {
-      throw new Error('MiddlewareInterface cannot be directly constructed')
+      throw new Error(ErrorMessage.cannotConstructed(MiddlewareInterface.name))
     }
   }
 
   regist () {
-    throw new Error('Unimplemented method')
+    throw new Error(ErrorMessage.unimplementedMethod())
   }
 
   middleware () {
-    throw new Error('Unimplemented method')
+    throw new Error(ErrorMessage.unimplementedMethod())
   }
 }
